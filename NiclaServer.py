9d78c1b7c23c0097c88186d49a374e9ffa45e274
JustTjebs <tjebbe.van.bergen@gmail.com>
2024-06-04 20:48:30 +0200

    🦹🏿‍♀️🚓🔫👮🏻
    Added stop when server time out

--- NiclaServer.py
+++ NiclaServer.py
@@ -7,19 +7,19 @@ from machine import Pin
 toZumo = Pin("PA9", Pin.OUT_PP)
 
 # Configuration
-HOST = '192.168.1.100'  # This should be the private IP address of the Nicla
+HOST = '192.168.1.101'  # This should be the private IP address of the Nicla
 PORT = 9090  # or another port
 SSID = "ICIDU"
 KEY = "ICIDUAVANS"
 
-# Sensor initialization
+# Sensor/ camera initialization
 sensor.reset()
 sensor.set_pixformat(sensor.RGB565)
 sensor.set_framesize(sensor.QVGA)
 sensor.skip_frames(time=2000)
 clock = time.clock()
 
-def check_zero_one(input_string):
+def check_zero_one(input_string): # fucntion to send data to zumo
     toZumo.low()
     for char in input_string:
         time.sleep_us(500)
@@ -32,7 +32,7 @@ def check_zero_one(input_string):
     toZumo.high()
 
 
-def do_connect():
+def do_connect():   #funtion to connect to wifi network
     sta_if = network.WLAN(network.STA_IF)
     if not sta_if.isconnected():
         print('Connecting to network...')
@@ -44,13 +44,14 @@ def do_connect():
             pass
     print('Network config:', sta_if.ifconfig())
 
-def send_image(communication_socket):
+def send_image(communication_socket): #funtion to send image
     img = sensor.snapshot()
     img.rotation_corr(z_rotation=180)  # Rotate the image 180 degrees
     compressed_img = img.compress(quality=15)  # Compress the image
     communication_socket.send(compressed_img)
     communication_socket.send(b'END_OF_IMAGE')
     print("Sent image")
+    communication_socket.send(b'END_OF_IMAGE') #extra end of image if the first one didn't work
 
 
 # Connect to the network
@@ -65,29 +66,34 @@ print(HOST)
 
 while True:
     print(f"waiting for socket")
-    communication_socket, address = server.accept()
+    communication_socket, address = server.accept() #accept conection from client
     print(f"Connected to {address}")
-    try:
-        message = communication_socket.recv(1024).decode('utf-8')  # 1024 is the buffer size
+    try: #if there is a socket connectet than
+        message = communication_socket.recv(1024).decode('utf-8')  #recieve message from client with a 1024 buffer
         print(f"Message from client: {message}")
-        message = str(message)
+        message = str(message)  #convert message to string
+                                #Nessisary for the check_zero_one funtion
 
         if message == 'image':
-            send_image(communication_socket)
+            send_image(communication_socket) # send image to client
 #            inputcode = "10010000"
 #            check_zero_one(inputcode)
 
         elif message == 'disconnect':
             print("Disconnecting")
 
-        elif message == 'stop':
+        elif message == 'stop': #when there is a time out
             print("stop")
+            input = "100000000"
+            check_zero_one(input)
 
         else:
-            check_zero_one(message)
+            check_zero_one(message) #send message to zumo
+            time.sleep_us(2)
 
     except Exception as e:
-        print(f"Exception: {e}")
+        print(f"Exception: {e}") #print exception
+        communication_socket.close()
     finally:
 #        print(f"communication_socket closed")
-        communication_socket.close()
+        communication_socket.close() #close communication_socket
